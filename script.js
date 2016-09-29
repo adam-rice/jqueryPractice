23fdb1c5da922f88cf98c4736613cca08ca9e40d
Adam Rice <adamcoltonrice@gmail.com>
2016-09-28 20:56:12 -0600

    adding functionality

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $('#button-one').on('click' , function() {
+    $('#panel-one').slideToggle(800);
+  });
+  $('#button-two').on('click' , function() {
+    $('#panel-two').slideToggle(800);
+  });
+  $('#button-three').on('click' , function() {
+    $('#panel-three').slideToggle(800);
+  });
+  $('#button-four').on('click' , function() {
+    $('#panel-four').slideToggle(800);
+  });
+});
